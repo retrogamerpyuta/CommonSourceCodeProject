56d96dd99b298c98a7ff3397d973e2c9f2b75451
emergencyqq <jsuzuki@resuscitate.jp>
2026-09-17 04:35:18 +0900

    EmuZ-80kのテンポ調整を行った

--- src/vm/mz80k/memory.cpp
+++ src/vm/mz80k/memory.cpp
@@ -162,7 +162,7 @@ void MEMORY::initialize()
 	// register event
 	register_vline_event(this);
 	register_event_by_clock(this, EVENT_TEMPO, CPU_CLOCKS / 96, true, NULL);	// 32hz * 3 Adjusted CPU clock to match the original MZ-80 timing
-	register_event_by_clock(this, EVENT_BLINK, CPU_CLOCKS / 3, true, NULL);		// 1.5hz * 2
+	register_event_by_clock(this, EVENT_BLINK, CPU_CLOCKS / 3, true, NULL);		// 1.5hz * 2 no change
 }
 
 void MEMORY::reset()
